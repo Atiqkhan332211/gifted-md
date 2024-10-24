fde0b6034799129143dca8349f5d95b5e262e110
Atiqkhan332211 <atiqk0519@gmail.com>
2024-10-25 01:08:51 +0500

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Gifted~yiwyDzwU",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~1Swbp5xw",
   PREFIX: process.env.PREFIX || ',',
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS !== undefined ? process.env.AUTO_READ_STATUS === 'true' : true, 
   AUTO_DOWNLOAD: process.env.AUTO_DOWNLOAD !== undefined ? process.env.AUTO_DOWNLOAD === 'true' : false,
@@ -19,7 +19,7 @@ const config = {
   AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.AUTO_REJECT_CALLS === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "SWATI ᴛᴇᴄʜ",
+  OWNER_NAME: process.env.OWNER_NAME || "GIFTED ᴛᴇᴄʜ",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "923409068757",
   GEMINI_KEY: process.env.GEMINI_KEY || "HRKU-90b3f81f-ad23-4861-8f87-e0edf3479972",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
